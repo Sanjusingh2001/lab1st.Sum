c2f1eb734948c9f584d10ed6cd86d406d1a3e4e5
hp <hp@DESKTOP-75BFR6A>
2023-08-21 22:34:42 +0530

    Add project files.

---
+++ lab1st.Sum/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1st.Sum
+{
+    class Sum
+    {
+        static void Main(string[] args)
+        {
+            int[] Arr = new int[5];
+            Console.WriteLine("Enter the element of array : ");
+            for (int i = 0; i < 5; i++) Arr[i] = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                sum += Arr[i];
+            }
+            Console.WriteLine("The sum of all elements of array : " + sum);
+            Console.ReadLine();
+        }
+    }
+}
